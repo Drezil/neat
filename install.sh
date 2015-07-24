75f3a3d5d23e00746076d4048c8600c2ce9c2f5b
Alex Wede <kontakt@alex-wede.de>
2015-07-24 21:06:41 +0200

    time version 1.5 und reorder-goals in install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ git clone https://github.com/Drezil/yesod-auth-oauth2
 cd ..
 cabal sandbox init
 cabal sandbox add-source deps/yesod-auth-oauth2
-cabal install --only-dependencies
+cabal install --only-dependencies --reorder-goals
