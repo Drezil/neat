ac00ceebefd29f80ff6a49537fe3e1711166d7dc
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2015-08-07 02:34:45 +0200

    made rough stock, without grouping. need esqueleto for that.

--- Handler/Stock.hs
+++ Handler/Stock.hs
@@ -3,4 +3,19 @@ module Handler.Stock where
 import Import
 
 getStockR :: Handler Html
-getStockR = error "Not yet implemented: getStockR"
+getStockR = loginOrDo (\(uid,user) -> do
+              items <- runDB $ selectList [TransactionUser ==. uid, TransactionInStock >. 0] [Asc TransactionTypeName]
+              defaultLayout $ [whamlet|
+             <h1>Current Stock
+             <table>
+               <tr>
+                 <th>Item name
+                 <th>Quantity
+                 <th>Buy Price
+               $forall Entity _ t <- items
+                 <tr>
+                   <td>#{transactionTypeName t}
+                   <td>#{transactionInStock t}
+                   <td>#{transactionPriceCents t}
+             |]
+            )
