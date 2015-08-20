7de15e9bacae379e7b9aa45f583eb77da730eed3
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2015-08-20 18:42:53 +0200

    added Handler/ProfitItems

--- Application.hs
+++ Application.hs
@@ -36,6 +36,7 @@ import Handler.Wallet
 import Handler.Settings
 import Handler.Update
 import Handler.Stock
+import Handler.ProfitItems
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/ProfitItems.hs
@@ -0,0 +1,6 @@
+module Handler.ProfitItems where
+
+import Import
+
+getProfitItemsR :: Handler Html
+getProfitItemsR = error "Not yet implemented: getProfitItemsR"
