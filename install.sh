6dd07ea798f03afba5196b65b700cc1355a26fae
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2016-01-08 06:42:07 +0100

    converted to stack, using lts-3.16

--- install.sh
+++ install.sh
@@ -5,7 +5,4 @@ cd deps
 git clone https://github.com/Drezil/yesod-auth-oauth2
 git clone https://github.com/Drezil/eve-api
 cd ..
-cabal sandbox init
-cabal sandbox add-source deps/yesod-auth-oauth2
-cabal sandbox add-source deps/eve-api
-cabal install --only-dependencies --reorder-goals
+#cabal install --only-dependencies --reorder-goals
